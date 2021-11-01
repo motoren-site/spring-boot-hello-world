077da9a1016fa9bda067a97ec86f74e2e0fcec28
Richard Haugen Olsen <einetunet@gmail.com>
2021-11-01 21:29:15 +0100

    Update HelloWorldController.java

--- src/main/java/no/nordnes/nykirken/hello/controller/HelloWorldController.java
+++ src/main/java/no/nordnes/nykirken/hello/controller/HelloWorldController.java
@@ -9,8 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api")
 @RestController
 public class HelloWorldController {
-
-
+    
     @GetMapping("/hello")
     public String hello() {
         log.info("Hello......");
