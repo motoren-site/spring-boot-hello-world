890c22ae0fd3a36ed7287c71f5b9a718256ecc69
Richard Haugen Olsen <einetunet@gmail.com>
2021-11-01 20:34:33 +0100

    Update HelloWorldController.java

--- src/main/java/no/nordnes/nykirken/hello/controller/HelloWorldController.java
+++ src/main/java/no/nordnes/nykirken/hello/controller/HelloWorldController.java
@@ -13,7 +13,7 @@ public class HelloWorldController {
 
     @GetMapping("/hello")
     public String hello() {
-        log.info("Hello....");
+        log.info("Hello.....");
         return "hello";
     }
 
