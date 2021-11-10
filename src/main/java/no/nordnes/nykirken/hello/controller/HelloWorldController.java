8b5bf125209c0b2aff4728d227d563d860449e08
Richard <einetunet@gmail.com>
2021-11-10 23:03:45 +0100

    Update HelloWorldController.java

--- src/main/java/no/nordnes/nykirken/hello/controller/HelloWorldController.java
+++ src/main/java/no/nordnes/nykirken/hello/controller/HelloWorldController.java
@@ -14,7 +14,7 @@ import java.time.format.DateTimeFormatter;
 @RestController
 public class HelloWorldController {
 
-    @GetMapping("/hello")
+    @GetMapping("/")
     public String hello() {
         log.info("Hello......");
         String hostName = System.getenv("HOSTNAME");
