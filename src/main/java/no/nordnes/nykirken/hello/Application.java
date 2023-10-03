db17fdcebe21926c0bd0db30278abb74ac51c32c
Richard Haugen Olsen <richard.haugen.olsen@miles.no>
2023-10-03 21:49:16 +0200

    ...

--- src/main/java/no/nordnes/nykirken/hello/Application.java
+++ src/main/java/no/nordnes/nykirken/hello/Application.java
@@ -1,11 +1,9 @@
 package no.nordnes.nykirken.hello;
 
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-@Slf4j
 @EnableScheduling
 @SpringBootApplication
 public class Application {
--- src/main/java/no/nordnes/nykirken/hello/controller/HelloWorldController.java
+++ src/main/java/no/nordnes/nykirken/hello/controller/HelloWorldController.java
@@ -1,6 +1,5 @@
 package no.nordnes.nykirken.hello.controller;
 
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,14 +8,12 @@ import java.time.LocalDate;
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
 
-@Slf4j
 @RequestMapping("/")
 @RestController
 public class HelloWorldController {
 
     @GetMapping("/")
     public String hello() {
-        log.info("Hello......");
         String hostName = System.getenv("HOSTNAME");
         return "<html><body><h1>Hallo fra " + hostName +".. (Spring Boot!)</h1>" +
                 "<p>Klokken er: " + getTime() +"</p>" +
